fa0d7e94931d315d6dbebf898384d5b17e7b8679
000hcl <mnrjohansson@gmail.com>
2026-06-06 12:38:26 +0300

    ex 11

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,14 +22,25 @@ router.post('/', tokenExtractor, async (req, res) => {
 })
 
 router.delete('/:id', blogFinder, tokenExtractor, async (req, res) => {
-  const blog = req.blog
-  if (blog) {
-    console.log(blog.toJSON())
-    await blog.destroy()
-    res.status(204).end()
-  } else {
-    res.status(404).end()
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = req.blog
+    if (user && user.id === blog?.userId) {
+      if (blog) {
+        console.log(blog.toJSON())
+        await blog.destroy()
+        res.status(204).end()
+      } else {
+        res.status(404).end()
+      }
+    } else {
+      res.status(401).json({ error: 'Unauthorized' })
+    }
+
+  } catch (error) {
+    return res.status(400).json({ error })
   }
+
 })
 
 router.put('/:id', blogFinder, async (req, res) => {
