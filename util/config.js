4850db52b278ff7f79e48823bcebf181ca1a1206
000hcl <mnrjohansson@gmail.com>
2026-06-06 21:07:36 +0300

    try to fix actions error

--- util/config.js
+++ util/config.js
@@ -4,6 +4,9 @@ const DATABASE_URL = process.env.TESTING
    ? process.env.TEST_DATABASE_URL
   : process.env.DATABASE_URL
 
+//for github actions:
+DATABASE_URL = DATABASE_URL ? DATABASE_URL : process.env.TEST_DATABASE_URL
+
 module.exports = {
   DATABASE_URL,
   PORT: process.env.PORT || 3001,
