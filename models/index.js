3035cb0b533c7cc8aac7c069a05bcdb7fe7a8141
000hcl <mnrjohansson@gmail.com>
2026-06-06 21:19:19 +0300

    jesus take the wheel

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
 
+User.hasMany(Blog)
+Blog.belongsTo(User)
 
 User.sync({ alter: true })
 Blog.sync({ alter: true })
 
-
-User.hasMany(Blog)
-Blog.belongsTo(User)
 
 module.exports = {
   Blog,
